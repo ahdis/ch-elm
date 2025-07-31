751a6cab42e1d6c38ec8cd86361eef744d564e21
oliveregger <oliver.egger@ahdis.ch>
2025-07-31 16:45:44 +0200

    Declare FOPH code system on IG Fixes #167

--- tests/updateterminology.sh
+++ tests/updateterminology.sh
@@ -660,6 +660,15 @@ curl --request GET \
   --key $KEY \
   -o ../input/resources/ConceptMap-$CONCEPTMAP.json
 
+CODESYSTEM="ch-elm-foph-code-reserve"
+curl --request GET \
+  --url https://ws.infreport-a.bag.admin.ch/ch-elm/v1/fhir/CodeSystem/$CODESYSTEM \
+  --header 'accept: application/fhir+json' \
+  --header 'user-agent: vscode-restclient' \
+  --cert $CERT \
+  --key $KEY \
+  -o ../input/resources/CodeSystem-$CODESYSTEM.json
+
 CODESYSTEM="ch-elm-foph-business-rules"
 curl --request GET \
   --url https://ws.infreport-a.bag.admin.ch/ch-elm/v1/fhir/CodeSystem/$CODESYSTEM \
